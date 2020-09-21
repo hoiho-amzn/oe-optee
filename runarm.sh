53882b80da059429fd1ac8554e5c4f6ae6618348
Donald Chan <hoiho@lab126.com>
2020-09-21 09:50:40 -0700

    fix: Re-arrange QEMU options in runarm.sh

--- runarm.sh
+++ runarm.sh
@@ -6,14 +6,14 @@ deploy=build/tmp-glibc/deploy/images/qemu-optee32
 HOSTFWD=
 
 ${root}/usr/bin/qemu-system-arm \
-	-s -S \
 	-nographic \
-	-machine virt,secure=on \
-	-cpu cortex-a15 \
-	-bios $deploy/optee-image-qemu-optee32.bios \
 	-serial tcp:localhost:54320 -serial tcp:localhost:54321 \
-	-m 1058 \
+	-s -S -machine virt,secure=on -cpu cortex-a15 \
+	-object rng-random,filename=/dev/urandom,id=rng0 \
+	-device virtio-rng-pci,rng=rng0,max-bytes=1024,period=1000 \
 	-netdev user,id=vmnic${HOSTFWD} -device virtio-net-device,netdev=vmnic \
+	-m 1057 \
+	-bios $deploy/optee-image-qemu-optee32.bios \
 	-device virtio-scsi-device,id=scsi \
 	-drive file=${deploy}/optee-image-qemu-optee32.ext4,id=rootimg,if=none,format=raw \
 	-device scsi-hd,drive=rootimg
